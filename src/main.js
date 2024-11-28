cf795da78f453335ef83429e5f523fd650b7c58e
Chayodom-Khruesuk <chayodom.kx@gmail.com>
2024-11-28 19:10:05 +0700

    add: package state for manage popup and set token

--- src/main.js
+++ src/main.js
@@ -6,10 +6,14 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
 axios.defaults.withCredentials = true;
+axios.defaults.headers.common['Authorization'] = `Bearer ${store.state.token}`
 
+Vue.use(VueSweetalert2)
 Vue.use(VueAxios, axios)
 Vue.use(require('vue-cookies'))
 Vue.config.productionTip = false
@@ -20,3 +24,11 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+axios.interceptors.request.use(config => {
+  const token = store.state.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,77 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     isLoggedIn: false,
-    username: null
+    username: null,
+    cart: JSON.parse(localStorage.getItem('cart')) || []
   },
   getters: {
+    getUsername: state => state.username,
+    isAuthenticated: state => !!state.username
   },
   mutations: {
+    setToken(state, token) {
+      state.token = token
+      localStorage.setItem('token', token)
+    },
     setUsername(state, username) {
       state.username = username;
+      state.isLoggedIn = !!username;
+    },
+    addToCart(state, item) {
+      const existingItem = state.cart.find(i => i._id === item._id)
+      if (existingItem) {
+        existingItem.quantity += item.quantity
+      } else {
+        state.cart.push(item)
+      }
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    },
+    clearCart(state) {
+        state.cart = [];
+        localStorage.setItem('cart', JSON.stringify(state.cart));
+    },
+    removeFromCart(state, item) {
+      state.cart = state.cart.filter(i => i._id !== item._id)
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    },
+    updateCartItemQuantity(state, item) {
+      const cartItem = state.cart.find(i => i._id === item._id)
+      if (cartItem) {
+        cartItem.quantity = parseInt(item.quantity)
+        localStorage.setItem('cart', JSON.stringify(state.cart))
+      }
     }
   },
   actions: {
+    initializeAuth({ commit }) {
+      const username = localStorage.getItem('username');
+      if (username) {
+        commit('setUsername', username);
+      }
+    },
     login({ commit }, username) {
       commit('setUsername', username);
+      localStorage.setItem('username', username);
+    },
+    logout({ commit }) {
+      commit('setUsername', null);
+      localStorage.removeItem('username');
     }
   },
-  modules: {
-  }
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: (key) => localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : null,
+        setItem: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+        removeItem: (key) => localStorage.removeItem(key),
+      },
+      paths: ['isLoggedIn', 'username'],
+    }),
+  ],
 })
